08c7dcaf2e7c3d6233ecb7a1ea68d2aa24b63069
Austin Semple <aus824@gmail.com>
2022-04-12 16:08:20 -0400

    Add files via upload

--- VendingMachine/src/main/java/Vending/vending/App.java
+++ VendingMachine/src/main/java/Vending/vending/App.java
@@ -1,13 +1,19 @@
+package Vending.vending;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Vending.vending;
-
 import Vending.controller.VendingController;
 import Vending.dao.VendingAuditDao;
+import Vending.dao.VendingAuditDaoFileImpl;
 import Vending.dao.VendingDao;
+import Vending.dao.VendingDaoFileImpl;
+import Vending.dao.VendingPersistenceException;
+import Vending.service.VendingInsufficientFundsException;
+import Vending.service.VendingNoItemInventoryException;
 import Vending.service.VendingServiceLayer;
+import Vending.service.VendingServiceLayerImpl;
 import Vending.ui.UserIO;
 import Vending.ui.UserIOConsoleImpl;
 import Vending.ui.VendingView;
@@ -18,12 +24,14 @@ import Vending.ui.VendingView;
  */
 public class App {
 
-    UserIO myIo = new UserIOConsoleImpl();
-    VendingView myView = new Vending.ui.VendingView();
-    VendingDao myDao = new Vending.dao.VendingDao();
-    VendingAuditDao myAuditDao = new Vending.dao.VendingAuditDaoFileImpl();
-    VendingServiceLayer myService = new Vending.service.VendingServiceLayerImpl(myDao, myAuditDao);
-    VendingController controller = new Vending.controller.VendingController(myService, myView);
-    controller.run()
+    public static void main(String[] args) throws VendingInsufficientFundsException, VendingNoItemInventoryException, VendingPersistenceException {
+        UserIO myIo = new UserIOConsoleImpl();
+        VendingView myView = new VendingView(myIo);
+        VendingDao myDao = new VendingDaoFileImpl();
+        VendingAuditDao myAuditDao = new VendingAuditDaoFileImpl();
+        VendingServiceLayer myService = new VendingServiceLayerImpl(myDao, myAuditDao);
+        VendingController controller = new VendingController(myService, myView);
+        controller.run();
+    }
 
 }
