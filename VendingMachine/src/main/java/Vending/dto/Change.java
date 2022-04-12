082ad43a59c9000ed0a9a0a7e43a033fe8e48e4b
Austin Semple <aus824@gmail.com>
2022-04-12 16:07:18 -0400

    Add files via upload

--- VendingMachine/src/main/java/Vending/dto/Change.java
+++ VendingMachine/src/main/java/Vending/dto/Change.java
@@ -22,7 +22,6 @@ enum Coins {
 //is then calculated into QUARTER, DIME, NICKEL, PENNY (enums) and sets private variables.
 //Setters and Getters to continue forward.
 
-//This class is static maybe? Because we'll only be using one change object
 public class Change {
     
     private BigDecimal quarter, dime, nickel, penny;
@@ -30,13 +29,13 @@ public class Change {
     enum Coins {
         QUARTER(25), DIME(10), NICKEL(5), PENNY(1);
         
-        private final int amount;
+        private final double amount;
 
-        private Coins(int amount) {
+        private Coins(double amount) {
             this.amount = amount;
         } 
         
-        public int getAmount(){
+        public double getAmount(){
             return amount;
         }
     }
@@ -57,29 +56,33 @@ public class Change {
         return quarter;
     }
     
-    private HashMap<String, BigDecimal> getChange(int pennies){
+    public HashMap<String, String> getChange(int pennies){
        
         BigDecimal bigPennies = new BigDecimal(pennies);
-        if(pennies >= Coins.QUARTER.getAmount()){
-            quarter = quarter.divide(bigPennies).setScale(0, RoundingMode.HALF_UP);
-            bigPennies = quarter.remainder(bigPennies).setScale(0,RoundingMode.HALF_UP);
-        }
-        if(pennies >= Coins.DIME.getAmount()){
-            dime = dime.divide(bigPennies).setScale(0, RoundingMode.HALF_UP);
-            bigPennies = dime.remainder(bigPennies).setScale(0,RoundingMode.HALF_UP);
-        }
-        if(pennies >= Coins.NICKEL.getAmount()){
-            nickel = nickel.divide(bigPennies).setScale(0, RoundingMode.HALF_UP);
-            bigPennies = nickel.remainder(bigPennies).setScale(0,RoundingMode.HALF_UP);
-        }
+        
+        
+       
+        quarter = bigPennies.divide(BigDecimal.valueOf(25.0)).setScale(0, RoundingMode.HALF_EVEN); 
+        bigPennies = bigPennies.remainder(BigDecimal.valueOf(25.0)).setScale(0, RoundingMode.HALF_EVEN);
+        
+        
+        dime = bigPennies.divide(BigDecimal.valueOf(10.0)).setScale(0, RoundingMode.HALF_EVEN); 
+        bigPennies = bigPennies.remainder(BigDecimal.valueOf(10.0)).setScale(0, RoundingMode.HALF_EVEN);
+        
+        
+        nickel = bigPennies.divide(BigDecimal.valueOf(5.0)).setScale(0, RoundingMode.HALF_EVEN); 
+        bigPennies = bigPennies.remainder(BigDecimal.valueOf(5.0)).setScale(0, RoundingMode.HALF_EVEN);
+        
+        
         penny = bigPennies;
         
-        HashMap<String, BigDecimal> changeMap = new HashMap<>();
-        changeMap.put("quarter", quarter);
-        changeMap.put("dime", dime);
-        changeMap.put("nickel", nickel);
-        changeMap.put("penny", penny);
-     
+        
+        HashMap<String, String> changeMap = new HashMap<>();
+        changeMap.put("quarter", quarter.toString());
+        changeMap.put("dime", dime.toString());
+        changeMap.put("nickel", nickel.toString());
+        changeMap.put("penny", penny.toString());
+       
         return changeMap;
     }
     
