37dae9364c12d808320eeae2d1bcf91753ac4f2d
Austin Semple <aus824@gmail.com>
2022-04-12 16:07:49 -0400

    Add files via upload

--- VendingMachine/src/main/java/Vending/service/VendingServiceLayer.java
+++ VendingMachine/src/main/java/Vending/service/VendingServiceLayer.java
@@ -4,10 +4,32 @@
  */
 package Vending.service;
 
+import Vending.dao.VendingPersistenceException;
+import Vending.dto.Item;
+import java.math.BigDecimal;
+import java.util.List;
 /**
  *
  * @author Austin
  */
 public interface VendingServiceLayer {
+    
+    Item chooseItem(String itemId, BigDecimal money) throws
+            VendingPersistenceException,
+            VendingInsufficientFundsException,
+            VendingNoItemInventoryException;
+    
+    
+    List<Item> displayItems() throws
+            VendingPersistenceException;
+    
+    void updateInventory(String itemId) throws
+            VendingPersistenceException;
+            
+    BigDecimal getItemCost(String itemId) throws 
+            VendingPersistenceException;
+    
+    int getItemInventory(String itemId) throws 
+            VendingPersistenceException;
     
 }
--- VendingMachine/src/main/java/Vending/service/VendingServiceLayerImpl.java
+++ VendingMachine/src/main/java/Vending/service/VendingServiceLayerImpl.java
@@ -6,22 +6,57 @@ package Vending.service;
 
 import Vending.dao.VendingAuditDao;
 import Vending.dao.VendingDao;
+import Vending.dao.VendingPersistenceException;
+import Vending.dto.Item;
+import java.math.BigDecimal;
+import java.util.List;
 
 /**
  *
  * @author Austin
  */
 public class VendingServiceLayerImpl implements VendingServiceLayer {
-    
+
     private VendingDao dao;
     private VendingAuditDao auditDao;
-    
-    public VendingServiceLayerImpl(VendingDao dao, VendingAuditDao auditDao){
+
+    public VendingServiceLayerImpl(VendingDao dao, VendingAuditDao auditDao) {
         this.dao = dao;
         this.auditDao = auditDao;
     }
+
+    @Override
+    public List<Item> displayItems() throws VendingPersistenceException {
+        return dao.displayItems();
+    }
+
+    @Override
+    public Item chooseItem(String itemId, BigDecimal money) throws VendingPersistenceException, VendingInsufficientFundsException, VendingNoItemInventoryException {
+        if(money.compareTo(dao.getItemCost(itemId)) == -1){
+            throw new VendingInsufficientFundsException("Invalid funds");
+        }
+        if(getItemInventory(itemId) <= 0){
+            throw new VendingNoItemInventoryException("No items left");
+        }
+        
+        auditDao.writeAuditEntry("Item: " + itemId + " was bought.");
+        return dao.chooseItem(itemId);
+    }
+
+    @Override
+    public void updateInventory(String itemId) throws VendingPersistenceException {
+        auditDao.writeAuditEntry("Item: " + itemId + " inventory was changed.");
+        dao.updateInventory(itemId);
+    }
     
+    @Override
+    public BigDecimal getItemCost(String itemId) throws VendingPersistenceException{
+       return dao.getItemCost(itemId);
+    }
     
-    
+    @Override
+    public int getItemInventory(String itemId) throws VendingPersistenceException{
+        return dao.getItemInventory(itemId);
+    }
     
 }
