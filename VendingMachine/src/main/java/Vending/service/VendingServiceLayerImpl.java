83259c267adbf1aeaf54ba6eda1ee586552c49cf
Austin Semple <aus824@gmail.com>
2022-04-13 10:06:53 -0400

    Update VendingServiceLayerImpl.java
    service layer comments

--- VendingMachine/src/main/java/Vending/service/VendingServiceLayerImpl.java
+++ VendingMachine/src/main/java/Vending/service/VendingServiceLayerImpl.java
@@ -25,11 +25,14 @@ public class VendingServiceLayerImpl implements VendingServiceLayer {
         this.auditDao = auditDao;
     }
 
+    //Displays items by returning the dao method
     @Override
     public List<Item> displayItems() throws VendingPersistenceException {
         return dao.displayItems();
     }
-
+    
+    //Finds exceptions for if user does not have enough funds by doing bigdecimal comparison. Also throws exception
+    //if vending machine does not have enough product. Adds an audit entry that an item was bought
     @Override
     public Item chooseItem(String itemId, BigDecimal money) throws VendingPersistenceException, VendingInsufficientFundsException, VendingNoItemInventoryException {
         if(money.compareTo(dao.getItemCost(itemId)) == -1){
@@ -43,17 +46,20 @@ public class VendingServiceLayerImpl implements VendingServiceLayer {
         return dao.chooseItem(itemId);
     }
 
+    //Updates .txt file with updated inventory. Adds an audit to audit file that an update was made
     @Override
     public void updateInventory(String itemId) throws VendingPersistenceException {
         auditDao.writeAuditEntry("Item: " + itemId + " inventory was changed.");
         dao.updateInventory(itemId);
     }
     
+    //Gets item cost by calling dao method
     @Override
     public BigDecimal getItemCost(String itemId) throws VendingPersistenceException{
        return dao.getItemCost(itemId);
     }
     
+    //gets amount of inventory of an item through dao method
     @Override
     public int getItemInventory(String itemId) throws VendingPersistenceException{
         return dao.getItemInventory(itemId);
