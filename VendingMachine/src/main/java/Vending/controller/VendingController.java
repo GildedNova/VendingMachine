f4eacfecf62bc08a3d762d59c2f7aa2c7193dbab
Austin Semple <aus824@gmail.com>
2022-04-13 10:01:20 -0400

    Update VendingController.java
    controller comments

--- VendingMachine/src/main/java/Vending/controller/VendingController.java
+++ VendingMachine/src/main/java/Vending/controller/VendingController.java
@@ -26,20 +26,22 @@ public class VendingController {
     private VendingView view;
     private final UserIO io = new UserIOConsoleImpl();
 
+    //Dependency injection
     public VendingController(VendingServiceLayer dao, VendingView view) {
         this.service = dao;
         this.view = view;
     }
     
     
-
+    
     public void run() throws VendingInsufficientFundsException, VendingNoItemInventoryException, VendingPersistenceException {
         boolean keepGoing = true;
         int menuSelection = 0;
-
+    
         BigDecimal money = getMoney();
         Change change = new Change();
         
+        //Main program loop, used to get user selection and process choice
         try {
             while (keepGoing) {
                 menuSelection = getMenuSelection();
@@ -64,22 +66,29 @@ public class VendingController {
 
     }
 
+    //Calls view method to get user selection
     private int getMenuSelection() {
         return view.printMenuAndGetSelection();
     }
     
+    //Asks the user how much money they are placing into the vending machine. returns amount in pennies as BigDecimal
     private BigDecimal getMoney() throws VendingPersistenceException{
        String amount = view.askUserForAmount();
        BigDecimal pennies = new BigDecimal(amount).multiply(BigDecimal.valueOf(100));
        return pennies;
     }
     
+    //Buys items from vending machine
     private void buyItem(BigDecimal money, Change change) throws VendingPersistenceException, VendingInsufficientFundsException, VendingNoItemInventoryException{
+        //Displays items to user
         List<Item> itemList = service.displayItems();
         view.displayItemList(itemList);
+        //Gets item id from user
         String itemId = view.getItemId();
+        //Processing the item and throws exceptions. Removes an item from .txt file, also adds an audit
         service.chooseItem(itemId, money);
         service.updateInventory(itemId);
+        //Processes amount of change left and shows the user
         money = money.subtract(service.getItemCost(itemId));
         view.displayMoney(money, change);
         
